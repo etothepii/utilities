7d93a85d26fb5a002d1a4502fa935e5147974d02
James Robinson <jr@epii.co.uk>
2016-04-10 15:42:30 +0100

    Added config to deploy to nexus thirdparty location

--- build/release.sh
+++ build/release.sh
@@ -5,16 +5,17 @@ if [[ $branch_name == release/* ]]; then
   minor_version=$(git tag | grep "v"$major_version | wc -l | tr -d '[[:space:]]')
   version=$major_version.$minor_version
   mvn versions:set -DnewVersion=$version 
-  mvn clean install
+  mvn clean test
   if [ $? -eq 0 ]; then
     version=v$version
     git checkout -b tag_$version
     git commit -a -m "Updated version numbers for $version"
     git tag $version
     git push origin $version
+    mvn deploy -Dmaven.test.skip=true
     git checkout $branch_name
     git branch -D tag_$version
   else
-    echo "Not tagging as install failed"
+    echo "Not tagging as tests failed"
   fi
 fi
