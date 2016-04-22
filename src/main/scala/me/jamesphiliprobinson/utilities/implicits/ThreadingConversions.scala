cef9fd111773521c307e1ea22e3949ad31a1a72f
James Robinson <jr@epii.co.uk>
2016-04-23 00:33:53 +0100

    minor refactoring which fixes #27

--- src/main/scala/me/jamesphiliprobinson/utilities/implicits/ThreadingConversions.scala
+++ src/main/scala/me/jamesphiliprobinson/utilities/implicits/ThreadingConversions.scala
@@ -4,5 +4,7 @@ package me.jamesphiliprobinson.utilities.implicits
   * Created by James Robinson on 22/04/2016.
   */
 object ThreadingConversions {
-  implicit def funcToRunnable( func : => Any ) = new Runnable { def run = func }
+  implicit def funcToRunnable(func: => Any) = new Runnable {
+    def run = func
+  }
 }
